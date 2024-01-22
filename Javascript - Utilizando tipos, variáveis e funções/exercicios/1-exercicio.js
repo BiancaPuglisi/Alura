27c002414f1bc2a2276728ebcbe5437d1756e13f
Bianca Puglisi <biancasnrpuglisi@gmail.com>
2024-01-22 08:38:31 -0300

    Inicio curso Javascript para backend

---
+++ Javascript - Utilizando tipos, variáveis e funções/exercicios/1-exercicio.js
@@ -0,0 +1,46 @@
+let name = 'Bianca';
+let number = 9;
+let boolean = true;
+console.log(typeof name);
+console.log(typeof number);
+console.log(typeof boolean);
+
+const nome = 'Bianca';
+const sobrenome = 'Silva Nunes Ribeiro Puglisi';
+const nomeSobrenome = nome + ' ' + sobrenome;
+const nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeSobrenome);
+console.log(nomeCompleto);
+
+let string = 'Bianca';
+let numero = 9;
+let frase = `9 é a resposta da ${string}`;
+console.log(frase);
+
+let variavel = 'Valor 1';
+console.log(variavel);
+variavel = 'Valor 2';
+console.log(variavel);
+
+var fora = 10;
+if(true){
+    var dentro = 5;
+}
+console.log(fora);
+console.log(dentro);
+
+//let fora = 10;
+//if(true){
+//    let dentro = 5;
+//}
+//console.log(fora);
+//console.log(dentro);
+
+const chuva = true;
+
+if (chuva) {
+    console.log('Leve um guarda-chuva!');
+} else {
+    console.log('Não é necessário guarda-chuva.');
+}
+
